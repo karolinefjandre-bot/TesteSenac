4d258723daed1e06f1750b96fd7f27879f19b1e9
Karoline <karolinefjandre@gmail.com>
2026-04-17 17:13:47 -0300

    Modificação

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int x = 50;
+    }
+}
